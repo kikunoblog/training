02ce6682ab2aa318661be27b797b8994052c75cf
kikunoblog <vikutowaarupisa@gmail.com>
2021-12-03 00:01:44 +0900

    2

---
+++ js/jQuery.js
@@ -0,0 +1,10 @@
+$(".js-menu").on("click", function(){
+$(".side").toggleClass("is-open");
+});
+$(".js-menu").on("click", function(){
+$(".l-container").toggleClass("is-open2");
+});
+$(".js-cross").on("click", function(){
+$(".side").removeClass("is-open");
+$(".l-container").removeClass("is-open2");
+});
