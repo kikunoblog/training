045ec9b40bf141f70e7ee901ad3a1e81aa3cd301
kikunoblog <vikutowaarupisa@gmail.com>
2021-12-05 19:51:59 +0900

    5

--- js/test.js
+++ js/test.js
@@ -1,12 +1,12 @@
-$(function(){
-$(".js-menu").on("click", function(){
-$(".p-side").toggleClass("is-open");
-});
-$(".js-menu").on("click", function(){
-$(".l-container").toggleClass("is-open2");
-});
-$(".js-cross").on("click", function(){
-$(".p-side").removeClass("is-open");
-$(".l-container").removeClass("is-open2");
-});
+$(function () {
+  $(".js-menu").on("click", function () {
+    $(".p-side").toggleClass("is-open");
+  });
+  $(".js-menu").on("click", function () {
+    $(".l-container").toggleClass("is-open2");
+  });
+  $(".js-cross").on("click", function () {
+    $(".p-side").removeClass("is-open");
+    $(".l-container").removeClass("is-open2");
+  });
 });
