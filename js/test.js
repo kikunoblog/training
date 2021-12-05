4c195253255d31b1923915e7e9052ab015e2b8be
kikunoblog <vikutowaarupisa@gmail.com>
2021-12-05 10:13:44 +0900

    5

--- js/test.js
+++ js/test.js
@@ -1,12 +1,12 @@
 $(function(){
 $(".js-menu").on("click", function(){
-$(".side").toggleClass("is-open");
+$(".p-side").toggleClass("is-open");
 });
 $(".js-menu").on("click", function(){
 $(".l-container").toggleClass("is-open2");
 });
 $(".js-cross").on("click", function(){
-$(".side").removeClass("is-open");
+$(".p-side").removeClass("is-open");
 $(".l-container").removeClass("is-open2");
 });
 });
